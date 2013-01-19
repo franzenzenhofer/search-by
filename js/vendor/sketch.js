2da56b7d6249b0d775bd707d8482b5fa09acb2ac
franz enzenhofer <f.enzenhofer@gmail.com>
2013-01-19 22:24:24 +0100

    quite ok version

---
+++ js/vendor/sketch.js
@@ -0,0 +1,217 @@
+//fork by franz enzenhofer
+//click now leads directly to a draw action
+//user can - for a moment - leave the canvas and come back 
+//changes can be found here https://github.com/intridea/sketch.js/pull/6
+var __slice = Array.prototype.slice;
+(function($) {
+  var Sketch;
+  $.fn.sketch = function() {
+    var args, key, sketch;
+    key = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+    if (this.length > 1) {
+      $.error('Sketch.js can only be called on one element at a time.');
+    }
+    sketch = this.data('sketch');
+    if (typeof key === 'string' && sketch) {
+      if (sketch[key]) {
+        if (typeof sketch[key] === 'function') {
+          return sketch[key].apply(sketch, args);
+        } else if (args.length === 0) {
+          return sketch[key];
+        } else if (args.length === 1) {
+          return sketch[key] = args[0];
+        }
+      } else {
+        return $.error('Sketch.js did not recognize the given command.');
+      }
+    } else if (sketch) {
+      return sketch;
+    } else {
+      this.data('sketch', new Sketch(this.get(0), key));
+      return this;
+    }
+  };
+  Sketch = (function() {
+    function Sketch(el, opts) {
+      this.el = el;
+      this.canvas = $(el);
+      this.context = el.getContext('2d');
+      this.options = $.extend({
+        toolLinks: true,
+        defaultTool: 'marker',
+        defaultColor: '#000000',
+        defaultSize: 5
+      }, opts);
+      this.painting = false;
+      this.mouse_in_out = false;
+      this.color = this.options.defaultColor;
+      this.size = this.options.defaultSize;
+      this.tool = this.options.defaultTool;
+      this.actions = [];
+      this.action = [];
+      this.canvas.bind('click mousedown mouseenter mouseup mousemove mouseleave mouseout touchstart touchmove touchend touchcancel', this.onEvent);
+      if (this.options.toolLinks) {
+        $('body').delegate("a[href=\"#" + (this.canvas.attr('id')) + "\"]", 'click', function(e) {
+          var $canvas, $this, key, sketch, _i, _len, _ref;
+          $this = $(this);
+          $canvas = $($this.attr('href'));
+          sketch = $canvas.data('sketch');
+          _ref = ['color', 'size', 'tool'];
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            key = _ref[_i];
+            if ($this.attr("data-" + key)) {
+              sketch.set(key, $(this).attr("data-" + key));
+            }
+          }
+          if ($(this).attr('data-download')) {
+            sketch.download($(this).attr('data-download'));
+          }
+          return false;
+        });
+      }
+    }
+    Sketch.prototype.download = function(format) {
+      var mime;
+      format || (format = "png");
+      if (format === "jpg") {
+        format = "jpeg";
+      }
+      mime = "image/" + format;
+      return window.open(this.el.toDataURL(mime));
+    };
+    Sketch.prototype.set = function(key, value) {
+      this[key] = value;
+      return this.canvas.trigger("sketch.change" + key, value);
+    };
+    Sketch.prototype.startPainting = function() {
+      this.painting = true;
+      return this.action = {
+        tool: this.tool,
+        color: this.color,
+        size: parseFloat(this.size),
+        events: []
+      };
+    };
+    Sketch.prototype.stopPainting = function() {
+      if (this.action) {
+        this.actions.push(this.action);
+      }
+      this.painting = false;
+      this.action = null;
+      return this.redraw();
+    };
+    Sketch.prototype.onEvent = function(e) {
+      if (e.originalEvent && e.originalEvent.targetTouches) {
+        e.pageX = e.originalEvent.targetTouches[0].pageX;
+        e.pageY = e.originalEvent.targetTouches[0].pageY;
+      }
+      $.sketch.tools[$(this).data('sketch').tool].onEvent.call($(this).data('sketch'), e);
+      e.preventDefault();
+      return false;
+    };
+    Sketch.prototype.redraw = function() {
+      var sketch;
+      this.el.width = this.canvas.width();
+      this.context = this.el.getContext('2d');
+      sketch = this;
+      $.each(this.actions, function() {
+        if (this.tool) {
+          return $.sketch.tools[this.tool].draw.call(sketch, this);
+        }
+      });
+      if (this.painting && this.action) {
+        return $.sketch.tools[this.action.tool].draw.call(sketch, this.action);
+      }
+    };
+    return Sketch;
+  })();
+  $.sketch = {
+    tools: {}
+  };
+  $.sketch.tools.marker = {
+    onEvent: function(e) {
+      //console.log(e.type);
+      switch (e.type) {
+        case 'mousedown':
+        case 'touchstart':
+          this.startPainting();
+          break;
+        case 'mouseup':
+        //case 'mouseout':
+        //case 'mouseleave':
+        case 'touchend':
+        case 'touchcancel':
+          this.stopPainting();
+          break;
+        case 'mouseenter':
+          //console.log('this.mouse_in_out:'+this.mouse_in_out);
+          if (this.mouse_in_out == true)
+          {
+            this.startPainting();
+            this.mouse_in_out = false
+            //console.log('special case mouseenter');
+          }
+          else
+          {
+            //console.log('normal case mouse enter');
+          }
+          break;
+        case 'mouseout':
+        case 'mouseleave':
+          if (this.painting == true)
+          {
+            //console.log('start mouse in out countdown')
+            this.mouse_in_out = true;
+            //console.log('this.mouse_in_out:'+this.mouse_in_out);
+            var that = this;
+            window.setTimeout(function(){
+            //console.log(that);
+            that.mouse_in_out = false;
+            //console.log('now it was too long outside')
+            }, 500);
+          }
+          this.stopPainting();
+          break;
+
+      }
+      if (this.painting) {
+        this.action.events.push({
+          x: e.pageX - this.canvas.offset().left,
+          y: e.pageY - this.canvas.offset().top,
+          event: e.type
+        });
+        return this.redraw();
+      }
+    },
+    draw: function(action) {
+      var event, previous, _i, _len, _ref;
+      this.context.lineJoin = "round";
+      this.context.lineCap = "round";
+      this.context.beginPath();
+      this.context.moveTo(action.events[0].x, action.events[0].y);
+      this.context.lineTo(action.events[0].x+0.1, action.events[0].y+0.11);
+      _ref = action.events;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        event = _ref[_i];
+        this.context.lineTo(event.x, event.y);
+        previous = event;
+      }
+      this.context.strokeStyle = action.color;
+      this.context.lineWidth = action.size;
+      return this.context.stroke();
+    }
+  };
+  return $.sketch.tools.eraser = {
+    onEvent: function(e) {
+      return $.sketch.tools.marker.onEvent.call(this, e);
+    },
+    draw: function(action) {
+      var oldcomposite;
+      oldcomposite = this.context.globalCompositeOperation;
+      this.context.globalCompositeOperation = "copy";
+      action.color = "rgba(0,0,0,0)";
+      $.sketch.tools.marker.draw.call(this, action);
+      return this.context.globalCompositeOperation = oldcomposite;
+    }
+  };
+})(jQuery);
--- server.coffee
+++ server.coffee
@@ -1,6 +1,7 @@
 #setting up express and the http server
 express = require('express')
 http = require('http')
+url = require('url')
 app = express();
 server = http.createServer(app)
 port = if process.env.PORT then process.env.PORT else 3000
@@ -8,7 +9,6 @@ port = if process.env.PORT then process.env.PORT else 3000
 #setting up socker.io
 io = require('socket.io').listen(server)
 knox = require('knox')
-
 
 #setting up the knox client for uplaod to S3
 crypto = require('crypto')
@@ -21,22 +21,43 @@ knox_client = knox.createClient(
 console.log(knox_client)
 
 
+
+
 #express stuff      
 app.use(express.static(__dirname + '/public'));
 app.enable('trust proxy')
+
+
 
 #commented out, as we are currently delivering the static index.html
 #app.get('/', (req, res) ->
 #  res.send('Hello World')
 #)
 
+
+#as google uses X-Frame-Options: SAMEORIGIN we can not embed the result in an iframe
+#app.set('view engine', 'jade');
+#app.set('view options', {
+#    layout: false
+#});
+#app.get '/d/:param_imgid', (req, res) ->
+#  console.log(req)
+#  imgurl = 'https://searchbydrawing.s3.amazonaws.com/images/'+req.params.param_imgid+'.png'
+#  res.render('detail',
+#    title: "Search by Drawing - "+req.params.param_imgid
+#    imgurl: imgurl
+#    google_search_by_image_url: 'https://www.google.com/searchbyimage?nota=1&image_url='+encodeURIComponent(imgurl)
+#    )
+
+
+
 #socket io stuff
 io.sockets.on('connection', (socket) ->
-  socket.emit('news', { hello: 'world' })
+  #socket.emit('news', { hello: 'world' })
   
-  socket.on('my other event', (data) -> 
-    console.log(data)
-  )
+  #socket.on('my other event', (data) -> 
+  #  console.log(data)
+  #)
 
   socket.on('q', (data) ->
     console.log(data)
@@ -51,7 +72,15 @@ io.sockets.on('connection', (socket) ->
     req.on('response', (res) ->
       if res.statusCode is 200
           console.log('KNOX saved to %s', req.url)
-          socket.emit('upload success', imgurl: req.url)
+          #socket.emit('upload success', imgurl: req.url)
+          url_parts=url.parse(req.url)
+          console.log(url_parts)
+          imageid=url_parts.pathname.replace('/images/', '').replace('.png', '')
+          console.log('imageid:'+imageid)
+          socket.emit('upload success', 
+            imgurl: req.url
+            imgid: imageid
+          )
       else
           console.log('KNOX ERROR START')
           console.log(res)
